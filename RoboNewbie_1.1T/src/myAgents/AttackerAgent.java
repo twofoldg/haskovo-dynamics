becfd3495707742f24dc7ce0feeab4d197a9a5d6
Stanimir Kovachev <stanimir.kovachevv@gmail.com>
2023-11-14 18:47:51 +0200

    Changing goalkeeper and attacker team name and removing comments

--- RoboNewbie_1.1T/src/myAgents/AttackerAgent.java
+++ RoboNewbie_1.1T/src/myAgents/AttackerAgent.java
@@ -54,30 +54,14 @@ public class AttackerAgent {
 
   PlayMode pm;
 
-  /** A player is identified in the server by its player ID and its team name.
-   There are at most two teams an the field, and every agent of a single team
-   must have a unique player ID between 1 and 11.
-   If the identification works right, it is visualized in the monitor:
-   the robots on the field have either red or blue parts. An unidentified
-   robot has grey parts. */
   final String id = "2";
-  final String team = "Haskovo";
-  /** The "beam"-coordinates specify the robots initial position on the field.
-   The root of the global field coordinate system is in the middle of the
-   field, the system is right-handed. The x-axis points to the opponent goal,
-   so the initial position has a negative x-value to beam the robot on its own
-   half. The robot can be placed with an initial orientation given in the
-   variable beamRot, in degrees, counterclockwise relative to the x-axis. */
+  final String team = "Haskovo-Dynamics";
+
   final double beamX =    -0.5;
   final double beamY =     0;
   final double beamRot =   0;
   ServerCommunication sc;
 
-  /**
-   * Initialize the connection to the server, the internal used classes and
-   * their relations to each other, and create the robot at a specified position
-   * on the field.
-   */
   private void init() {
 
     sc = new ServerCommunication();
@@ -121,10 +105,6 @@ public class AttackerAgent {
     }
   }
 
-  /**
-   * Update the world and robot hardware informations, that means process
-   * perceptor values provided by the server.
-   */
   private void sense() {
     // Receive the server message and parse it to get the perceptor values.
     percIn.update();
@@ -191,11 +171,6 @@ public class AttackerAgent {
     }
   }
 
-  /**
-   * Move the robot hardware, that means send effector commands to the server.
-   *
-   * The cases should correspond to the cases in method think.
-   */
   private void act() {
     switch (pm) {
             /*
@@ -207,12 +182,6 @@ public class AttackerAgent {
             *
             */
       default:    // in all other game states except after
-        /**
-         * Calculate effector commands and send them to the server, this
-         * method of class KeyframeMotion has to be called in every
-         * server cycle where keyframe motions called by method think
-         * should be performed.
-         */
         kfMotion.executeKeyframeSequence();
         // No matter, which motion the robot executes, it should
         // always turn its head around. So the LookAroundMotion
--- RoboNewbie_1.1T/src/myAgents/GoalKeeperAgent.java
+++ RoboNewbie_1.1T/src/myAgents/GoalKeeperAgent.java
@@ -44,29 +44,12 @@ public class GoalKeeperAgent {
   private SoccerTeamThinking goalKeeperThinking;
   private LookAroundMotion lookAround;
 
-  /** A player is identified in the server by its player ID and its team name.
-   There are at most two teams an the field, and every agent of a single team
-   must have a unique player ID between 1 and 11.
-   If the identification works right, it is visualized in the monitor:
-   the robots on the field have either red or blue parts. An unidentified
-   robot has grey parts. */
   final String id = "1";
-  final String team = "simpleSoccer";
-  /** The "beam"-coordinates specify the robots initial position on the field.
-   The root of the global field coordinate system is in the middle of the
-   field, the system is right-handed. The x-axis points to the opponent goal,
-   so the initial position has a negative x-value to beam the robot on its own
-   half. The robot can be placed with an initial orientation given in the
-   variable beamRot, in degrees, counterclockwise relative to the x-axis. */
+  final String team = "Haskovo-Dynamics";
+
   final double beamX =    -4;
   final double beamY =     0;
   final double beamRot =   0;
-
-  /**
-   * Initialize the connection to the server, the internal used classes and
-   * their relations to each other, and create the robot at a specified position
-   * on the field.
-   */
 
   private SimpleGoalie goaltender;
 
@@ -86,16 +69,6 @@ public class GoalKeeperAgent {
     goaltender = new SimpleGoalie(kfMotion, percIn, log);
   }
 
-
-  /**
-   * Main loop of the agent program, where it is synchronized with the
-   * simulation server.
-   *
-   * How long the agent program will run can be changed in variable
-   * "agentRunTimeInSeconds". This is just an approximation, because this value
-   * is used to calculate a number of server cycles, and the agent will
-   * participate in this amount of cycles.
-   */
   public void run(){
 
     int agentRunTimeInSeconds = 1200;
@@ -123,10 +96,6 @@ public class GoalKeeperAgent {
     }
   }
 
-  /**
-   * Update the world and robot hardware informations, that means process
-   * perceptor values provided by the server.
-   */
   private void sense() {
     // Receive the server message and parse it to get the perceptor values.
     percIn.update();
@@ -134,18 +103,10 @@ public class GoalKeeperAgent {
     localView.update();
   }
 
-  /**
-   * Decide, what is sensible in the actual situation.
-   * Use the knowledge about the field situation updated in sense(), and choose
-   * the next movement - it will be realized in act().
-   */
   private void think(){
     goaltender.decide();
   }
 
-  /**
-   * Move the robot hardware, that means send effector commands to the server.
-   */
   private void act(){
     // Calculate effector commands and send them to the server, this method
     // of class KeyframeMotion has to be called in every server cycle.
@@ -157,9 +118,6 @@ public class GoalKeeperAgent {
     effOut.sendAgentMessage();
   }
 
-  /**
-   * Print log informations - if there where any.
-   */
   private void printlog() {
     log.printLog();
   }
